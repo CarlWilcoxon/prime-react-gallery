d71ba354360417f068ef2b92915f8cded676ca20
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-10 18:00:16 -0500

    Added a component template

---
+++ src/components/_template/_template.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './_template.css';
+
+class _template extends Component {
+
+  state = {
+
+  }
+  componentDidMount() {
+    //DO stuff on load;
+  }
+
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default _template;
