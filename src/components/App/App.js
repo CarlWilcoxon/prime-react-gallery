60ac24ed32f3b1896c5c19d1d64ec94922ca7704
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-10 17:54:08 -0500

    Added GET

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+
+  }
+  componentDidMount() {
+    this.getPics();
+  }
+
+  getPics = () => {
+
+    axios.get('/gallery')
+    .then( (response) => {
+      console.log( 'Got pics', response.data);
+      //this.setState({ TODO store the pictures })
+    }).catch ( (err) => {
+      console.log( 'failed to get pictures', err );
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +30,7 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" alt="a small goat"/>
       </div>
     );
   }
