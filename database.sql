9b78756dd156473acedad03191876f4998abdb27
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-12 16:28:29 -0500

    typo

--- database.sql
+++ database.sql
@@ -5,9 +5,9 @@ CREATE TABLE gallery (
 	likes INTEGER
 );
 
-INSERT INTO gallery (path, description, likes) 
+INSERT INTO gallery (path, description, likes)
 VALUES
-  ('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.', 0),
+  ('images/goat_small.jpg', 'A silly goat.', 0),
   ('images/malaysia_beaches.jpg', 'My best friend and I on the beaches of Malaysia.', 5),
   ('images/snowfall.jpeg', 'Me in Tokyo the night after we arrived, it snowed. One of my bags did not make it, so I was stuck in flip flops for a day.', 3),
   ('images/wrastling.jpeg',	'We also saw a pro wrestling match while we were in Japan', 37);
