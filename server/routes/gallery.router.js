6c14f53b7e3bd1832ea53673d87886fd471778aa
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-12 10:18:28 -0500

    bringing up to date

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -6,9 +6,13 @@ const pool = require('../modules/pool');
 // PUT Route
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
+
+    // Add a like to the passed table row
     const galleryId = req.params.id;
+
+    // add a like to the likes total on the table
     const sqlText = `UPDATE gallery
-                    SET likes = likes+1
+                    SET likes = likes + 1
                     WHERE id = $1`
     pool.query( sqlText, [galleryId] )
       .then( (result) => {
@@ -21,8 +25,46 @@ router.put('/like/:id', (req, res) => {
     })
 }); // END PUT Route
 
+// POST Route
+router.post('/', (req, res) => {
+  console.log(req.body);
+
+  // Create a new table entry with the passed object
+  const sqlText = `INSERT INTO gallery (path, description, likes)
+  VALUES ( $1, $2, $3 )`
+  pool.query(sqlText, [req.body.path, req.body.description, req.body.likes])
+    .then(( result ) => {
+      console.log('Added photo');
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+  })
+}); // END POST Route
+
+// DELETE Route
+router.delete('/delete/:id', (req, res) => {
+  console.log(req.params);
+
+  // DELETE the table entry with passed id
+  const galleryId = req.params.id;
+  const sqlText = `DELETE FROM gallery
+                  WHERE id = $1`
+  pool.query( sqlText, [galleryId] )
+    .then( (result) => {
+      console.log('Deleted picture #', galleryId);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+  })
+}); // END DELETE Route
+
 // GET Route
 router.get('/', (req, res) => {
+  // get all the pics and order them by their id number
   const sqlText = `SELECT * FROM gallery ORDER BY id`;
   pool.query(sqlText)
     .then(( result ) => {
